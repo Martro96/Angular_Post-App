3e972c19a6add154862b2778951b06fa46e02302
Martro96 <martarodrigo.55@gmail.com>
2025-03-26 12:25:51 +0100

    Actualizo Header

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { HeaderMenus } from 'src/app/Models/header-menus.dto';
-import { HeaderMenusService } from 'src/app/Services/header-menus.service';
-import { LocalStorageService } from 'src/app/Services/local-storage.service';
+
+import { select, Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { selectUserId } from 'src/app/Auth/reducers/auth.selectors';
+import { logout } from 'src/app/Auth/actions/auth.actions';
 
 @Component({
   selector: 'app-header',
@@ -12,26 +14,31 @@ import { LocalStorageService } from 'src/app/Services/local-storage.service';
 export class HeaderComponent implements OnInit {
   showAuthSection: boolean;
   showNoAuthSection: boolean;
+  private userId!: string;
 
   constructor(
     private router: Router,
-    private headerMenusService: HeaderMenusService,
-    private localStorageService: LocalStorageService
+    private store: Store<AppState> 
+
   ) {
     this.showAuthSection = false;
     this.showNoAuthSection = true;
   }
 
+
   ngOnInit(): void {
-    this.headerMenusService.headerManagement.subscribe(
-      (headerInfo: HeaderMenus) => {
-        if (headerInfo) {
-          this.showAuthSection = headerInfo.showAuthSection;
-          this.showNoAuthSection = headerInfo.showNoAuthSection;
-        }
+
+    this.store.pipe(select(selectUserId)).subscribe((userId) => {
+      this.userId = userId;
+      this.showAuthSection = !!userId;
+      this.showNoAuthSection = !userId; 
+
+      if (userId) {
+        this.userId = userId;
       }
-    );
+    })
   }
+  
 
   dashboard(): void {
     this.router.navigateByUrl('dashboard');
@@ -62,16 +69,7 @@ export class HeaderComponent implements OnInit {
   }
 
   logout(): void {
-    this.localStorageService.remove('user_id');
-    this.localStorageService.remove('access_token');
-
-    const headerInfo: HeaderMenus = {
-      showAuthSection: false,
-      showNoAuthSection: true,
-    };
-
-    this.headerMenusService.headerManagement.next(headerInfo);
-
-    this.router.navigateByUrl('home');
+    this.store.dispatch(logout())
+    this.router.navigateByUrl('home')
   }
 }
